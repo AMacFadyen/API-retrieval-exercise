5bdd04e9de0be5f4c005f7a69c99cb177b4cf9c4
AMacFadyen <ferretsbite@hotmail.co.uk>
2017-12-11 01:24:58 +0000

    displayed details of characters on site

--- public/app.js
+++ public/app.js
@@ -13,10 +13,39 @@ var makeRequest = function(url, callback){
 var requestComplete = function(){
     if(this.status != 200) return;
     var jsonString = this.responseText;
-    var studentList = JSON.parse(jsonString);
-    console.log(studentList);
-    populateHeader(studentList);
-    populateList(studentList.results);
+    var characterList = JSON.parse(jsonString);
+    console.log(characterList);
+    populateList(characterList);
 }
+
+var populateList = function(characters){
+    var mainSection = document.getElementById('main-section');
+    characters.forEach(function(character){
+        var section = document.createElement('section');
+        var header = document.createElement('h1');
+        var fact1 = document.createElement('h2');
+        var fact2 = document.createElement('h2');
+        var fact3 = document.createElement('h2');
+        var fact4 = document.createElement('h2');
+        var image = document.createElement('img');
+
+        header.innerText = character.name;
+        fact1.innerText = "School House: " + character.house;
+        fact2.innerText = "Gender: " + character.gender;
+        fact3.innerText = "Blood-Line: " + character.ancestry;
+        fact4.innerText = "Known Patroneous: " + character.patronus;
+        image.src = character.image;
+        image.className = "thumbnail";
+        image.alt = character.name;
+
+        section.appendChild(header);
+        section.appendChild(fact1);
+        section.appendChild(fact2);
+        section.appendChild(fact3);
+        section.appendChild(fact4);
+        section.appendChild(image);
+        mainSection.appendChild(section);
+    });
+};
 
 window.addEventListener('load', app);
